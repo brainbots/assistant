6b1ce3e19646618b8e0a37401d6256b062fce2cc
Mohamed Seleem <seleem1337@gmail.com>
2017-04-23 17:59:11 +0200

    Formatting

--- assisstant/keyboard/input/virtual.py
+++ assisstant/keyboard/input/virtual.py
@@ -1,20 +1,20 @@
 import time
 
 def read(_, no_of_seconds):
-    time.sleep(no_of_seconds)
+  time.sleep(no_of_seconds)
 
-    values={'O1':[],'O2':[],'P7':[],'P8':[]}
-    for i in range(no_of_seconds*128):
-        values['O1'].append(1)
-        values['O2'].append(2)
-        values['P7'].append(3)
-        values['P8'].append(4)
+  values={'O1':[],'O2':[],'P7':[],'P8':[]}
+  for i in range(no_of_seconds*128):
+    values['O1'].append(1)
+    values['O2'].append(2)
+    values['P7'].append(3)
+    values['P8'].append(4)
 
-    quality={'O1':[],'O2':[],'P7':[],'P8':[]}
-    for i in range(no_of_seconds*128):
-        quality['O1'].append(1)
-        quality['O2'].append(2)
-        quality['P7'].append(3)
-        quality['P8'].append(4)
+  quality={'O1':[],'O2':[],'P7':[],'P8':[]}
+  for i in range(no_of_seconds*128):
+    quality['O1'].append(1)
+    quality['O2'].append(2)
+    quality['P7'].append(3)
+    quality['P8'].append(4)
 
-    return (values, quality)
+  return (values, quality)
