9ab5e1077fb2977bcdaa87a465a2ad0e7305468a
Mohamed Seleem <seleem1337@gmail.com>
2017-04-23 17:04:13 +0200

    Remove prints() from keyboard/input/virtual

--- assisstant/keyboard/input/virtual.py
+++ assisstant/keyboard/input/virtual.py
@@ -1,9 +1,7 @@
 import time
 
 def read(_, no_of_seconds):
-    print("Started sleeping")
     time.sleep(no_of_seconds)
-    print("Stopped sleeping")
 
     values={'O1':[],'O2':[],'P7':[],'P8':[]}
     for i in range(no_of_seconds*128):
