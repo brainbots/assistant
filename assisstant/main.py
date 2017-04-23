f693f09bfedc4981557741a8ac445c160faab65d
Mohamed Seleem <seleem1337@gmail.com>
2017-04-23 17:58:00 +0200

    Add signal handler to quit the application

--- assisstant/main.py
+++ assisstant/main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import sys
+import signal
 from PyQt5.QtWidgets import QApplication
 from keyboard.ui.widgets import KeyboardWindow
 
 if __name__ == '__main__':
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
   app = QApplication([])
   window = KeyboardWindow()
   window.showMaximized()
