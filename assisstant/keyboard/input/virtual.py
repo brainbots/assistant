c21654568e5dda1523595060dc625da31500e4d6
Amr Mohamed <amr_mohamed@live.com>
2017-05-06 18:12:06 +0200

    Fix headset not reading packets

--- assisstant/keyboard/input/virtual.py
+++ assisstant/keyboard/input/virtual.py
@@ -1,7 +1,7 @@
 import time
 import numpy as np
 
-def read(_, no_of_seconds):
+def read(no_of_seconds):
   time.sleep(no_of_seconds)
   
   values = np.random.random_sample((4, no_of_seconds * 128))
