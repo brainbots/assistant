44b1cc797f03e986a013bf024240bc963ff94258
Mohamed Thabet <thabetx@gmail.com>
2017-05-11 16:28:03 +0200

    Add command line argument for the headset
    --headset or --virtual

--- assisstant/keyboard/manager.py
+++ assisstant/keyboard/manager.py
@@ -8,9 +8,11 @@ class Manager(QObject):
   flash_signal = pyqtSignal(bool)
   update_signal = pyqtSignal(int)
 
-  def __init__(self, parent=None):
+  def __init__(self, is_virtual, parent=None):
     super(Manager, self).__init__(parent)
-    self.device = device.Device(callback=self.device_update, collect_time=config.TIME_FLASH_SEC, is_virtual=False)
+    self.is_virtual = is_virtual
+    self.device = device.Device(callback=self.device_update,
+                                collect_time=config.TIME_FLASH_SEC, is_virtual=self.is_virtual)
     self.device.collect_signal.connect(self.device_update)
     self.paused = False
 
@@ -33,9 +35,10 @@ class Manager(QObject):
     self.flash_signal.emit(collecting)
     if not collecting:
       sample, _quality = data
-      result = cca.classify(sample, config.FREQ, config.TIME_FLASH_SEC) - 1
-      print(result)
-      #result = randint(0, 3)
+      if self.is_virtual:
+        result = randint(0, 3)
+      else:
+        result = cca.classify(sample, config.FREQ, config.TIME_FLASH_SEC) - 1
       self.update_signal.emit(result)
       if not self.paused:
         QTimer.singleShot(config.TIME_REST_SEC * 1000, Qt.PreciseTimer, self.device.collect)
