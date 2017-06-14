c5e5ed0d2e0fd681a66f74978b8fefc27a1b7777
Mo'men Adel <mouemened@gmail.com>
2017-06-14 15:57:58 +0200

    extract last two words in query sentence

--- assisstant/keyboard/autocomplete/backends/autocomplete.py
+++ assisstant/keyboard/autocomplete/backends/autocomplete.py
@@ -5,7 +5,10 @@ class AutoComplete:
 		autocomplete.load()
 
 	def predict(self, query):
-		*sentence, word = query.split(" ")
-		sentence = " ".join(sentence)
-		return autocomplete.predict(sentence, word)[:3]
+		*prev, word = query.split(" ")
+		if len(prev) > 1:
+			prev = prev[-1]
+		else:
+			prev = prev[0]
+		return autocomplete.predict(prev, word)[:3]
 
