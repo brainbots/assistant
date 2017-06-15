3556a8dd46010c7571cd76d6b25f8994289294a2
Amr Mohamed <amr_mohamed@live.com>
2017-06-15 02:21:19 +0200

    nlp/nlp_manager.py: Fix indentation prolems

--- assisstant/nlp/nlp_manager.py
+++ assisstant/nlp/nlp_manager.py
@@ -3,18 +3,18 @@ from nlp.backends.api_ai import ApiaiBackend
 from nlp.backends.nlp_backend import NlpBackend
 
 class NlpManager:
-	def __init__(self):
-		self.NlpBackend = ApiaiBackend()
-		# TODO: Session id should be related to the current user
-		self.session_id = 1
-		self.reset_contexts = True
+    def __init__(self):
+	    self.NlpBackend = ApiaiBackend()
+	    # TODO: Session id should be related to the current user
+	    self.session_id = 1
+	    self.reset_context = True
 
-	def get_intent(self, query):
-		return self.NlpBackend.get_intent(
-		            query,self.session_id,self.reset_contexts)
+    def get_intent(self, query):
+	    return self.NlpBackend.get_intent(
+		            query,self.session_id,self.reset_context)
 
     def keep_contexts(self):
-		self.reset_contexts = False
+	    self.reset_contexts = False
 
     def reset_contexts(self):
-		self.reset_contexts = True
+	    self.reset_contexts = True
