ab1042e663613f82bb842585f5af3594e51aec48
Mo'men Adel <mouemened@gmail.com>
2017-06-17 14:17:37 +0200

    temp handling for 1 word query

--- assisstant/keyboard/autocomplete/backends/autocomplete.py
+++ assisstant/keyboard/autocomplete/backends/autocomplete.py
@@ -8,7 +8,10 @@ class AutoComplete:
 		*prev, word = query.split(" ")
 		if len(prev) > 1:
 			prev = prev[-1]
-		else:
+		elif len(prev) == 1:
 			prev = prev[0]
+		else:
+			# TODO find another solution
+			prev = "the"
 		return autocomplete.predict(prev, word)[:3]
 
