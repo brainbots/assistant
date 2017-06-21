61aeccc99c68965679e6b8824155259e0d3edc13
Mohamed Seleem <seleem1337@gmail.com>
2017-06-21 17:05:34 +0200

    Convert strings to int

--- assisstant/bots/utility.py
+++ assisstant/bots/utility.py
@@ -2,10 +2,11 @@ import subprocess
 
 # criteria: dictionary that has key/values to match against.
 # e.g. {"wm_class": "Navigator.Firefox"}
-def getHWND(criteria):
+def getWindow(criteria):
   windows = getAllWindows()
   for window in windows:
     if criteria.items() <= window.items():
+      print(window)
       return window
   return None
 
@@ -15,5 +16,5 @@ def getAllWindows():
   with subprocess.Popen(["wmctrl", "-l", "-p", "-x"], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
     for line in p.stdout:
       tokens = line.split()
-      windows.append({"hwnd": tokens[0], "workspace": tokens[1], "pid": tokens[2], "wm_class": tokens[3], "title": " ".join(tokens[5:])})
+      windows.append({"hwnd": int(tokens[0], 16), "workspace": int(tokens[1]), "pid": int(tokens[2]), "wm_class": tokens[3], "title": " ".join(tokens[5:])})
   return windows
