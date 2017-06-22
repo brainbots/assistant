c3852b96888a74a5a0cb42345fbe51a16611d598
AhmedMostafaSoliman <ahmedmostafa72@hotmail.com>
2017-06-23 01:52:04 +0200

    Mail bot is added

--- assisstant/settings.py
+++ assisstant/settings.py
@@ -3,6 +3,7 @@
 REGISTERED_BOTS = [
 	'bots.bots.calculator.CalculatorBot',
 	'bots.bots.weather.WeatherBot',
-        'bots.bots.chrome.ChromeBot',
+	'bots.bots.chrome.ChromeBot',
+	'bots.bots.mail.MailBot',
 	# 'bots.bots.search.SearchBot',
 ]
