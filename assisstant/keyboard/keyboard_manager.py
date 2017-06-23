007d153839dc207da6a02b430aa4720cc56b3b0c
Mohamed Thabet <thabetx@gmail.com>
2017-06-24 00:33:19 +0200

    Minor adjustments and comments

--- assisstant/keyboard/keyboard_manager.py
+++ assisstant/keyboard/keyboard_manager.py
@@ -13,28 +13,33 @@ class KeyboardManager(QObject):
 
   def __init__(self, is_virtual, parent=None):
     super(KeyboardManager, self).__init__(parent)
+
     self.keyboard_window = KeyboardWindow()
     self.keyboard_window.showMaximized()
 
     self.autocomplete_manager = AutoCompleteManager()
+    self.is_virtual = is_virtual
 
     # self.keyboard_window.ui_pause.connect(self.pause_handler)
     # self.keyboard_window.ui_freeze.connect(self.freeze_handler)
-
     # self.keyboard_window.autocomplete_signal.connect(self.predict_word)
 
-    QTimer.singleShot(settings.TIME_REST_SEC * 1000, Qt.PreciseTimer, self.start)
-
-    self.is_virtual = is_virtual
     self.device = device.Device(callback=self.device_update,
                                 collect_time=settings.TIME_FLASH_SEC,
                                 is_virtual=self.is_virtual)
+
     self.device.collect_signal.connect(self.device_update)
+
+
     # self.freeze = False
     self.paused = False
     self.old_data = getUserDatasets()
+
+    # predetermined sequence of choices for testing
     self.seq = [1,1,1,1,3,3,3,3,3,3]
-    # self.seq = []
+
+    # initial delay
+    QTimer.singleShot(settings.TIME_REST_SEC * 1000, Qt.PreciseTimer, self.start)
 
   # def pause_handler(self, paused):
   #   self.paused = paused
@@ -76,7 +81,9 @@ class KeyboardManager(QObject):
           result = randint(0, 3)
       else:
         result = cca.classify(sample, settings.FREQ, settings.TIME_FLASH_SEC, self.old_data)
-      char = self.keyboard_window.update_handler(result)
+
+      char = self.keyboard_window.update(result)
+
       if char:
         self.char_selected(char)
       if not self.paused:
