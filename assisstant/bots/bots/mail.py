e084e3dae49d6b148230af89a0702b8588eb7a10
AhmedMostafaSoliman <ahmedmostafa72@hotmail.com>
2017-06-28 13:59:16 +0200

    mail bot bug fixes

--- assisstant/bots/bots/mail.py
+++ assisstant/bots/bots/mail.py
@@ -6,7 +6,7 @@ import settings
 
 class MailBot(AbstractBot):
     def __init__(self, id):
-        actions = ['mail.send']
+        actions = ['send']
         super().__init__(id, actions)
         # REQUIRED
         self.mail_from = None
@@ -15,30 +15,31 @@ class MailBot(AbstractBot):
         self.body = None
 
     def extract_attr(self, intent):
-        if not self.mail_from :
-            if intent.parameters['mail_from']:
-                self.mail_from = intent.parameters['mail_from']
-        if not self.mail_to :
-            if intent.parameters['mail_to']:
-                self.mail_to = intent.parameters['mail_to']
-        if not self.subject :
-            if intent.parameters['subject']:
-                self.mail_to = intent.parameters['subject']
-        if not self.body :
-            if intent.parameters['body']:
-                self.mail_to = intent.parameters['body']
+        if intent.parameters['from']:
+            self.mail_from = intent.parameters['from']
+        if intent.parameters['to']:
+            self.mail_to = intent.parameters['to']
+        if intent.parameters['subject']:
+            self.subject = intent.parameters['subject']
+        if intent.parameters['body']:
+            self.body = intent.parameters['body']
 
     def execute(self):
         sg = sendgrid.SendGridAPIClient(apikey=settings.SENDGRID_ACCESS_TOKEN)
-        from_email = Email(self.from_email)
-        to_email = Email(self.to_email)
+        from_email = Email(self.mail_from)
+        to_email = Email(self.mail_to)
         subject = self.subject
-        content = Content(self.body)
+        content = Content("text/plain",self.body)
         mail = Mail(from_email, subject, to_email, content)
         response = sg.client.mail.send.post(request_body=mail.get())
-        # print(response.status_code)
-        # print(response.body)
-        # print(response.headers)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+        return Action(
+            action_type='message',
+            body='email sent successfully',
+            bot=self.id,
+            keep_context=False)
 
     def is_long_running(self):
         return False
@@ -47,28 +48,28 @@ class MailBot(AbstractBot):
         return (self.mail_from == None or self.mail_to == None or self.subject == None or self.body == None)
 
     def request_missing_attr(self):
-        if not self.mail_from:
+        if self.mail_from == None:
             return Action(
                 action_type='inquiry',
                 body='Please specify the sender email address',
                 bot=self.id,
                 keep_context=True)
 
-        if not self.mail_to:
+        if self.mail_to == None:
             return Action(
                 action_type='inquiry',
                 body='Please specify the recipient email address',
                 bot=self.id,
                 keep_context=True)
 
-        if not self.subject:
+        if self.subject == None:
             return Action(
                 action_type='inquiry',
                 body='What is the mail subject?',
                 bot=self.id,
                 keep_context=True)
 
-        if not self.body:
+        if self.body == None:
             return Action(
                 action_type='inquiry',
                 body='What is the mail body ?',
