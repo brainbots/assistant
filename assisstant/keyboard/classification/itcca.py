bc99000423c115579ef13c9ad0050f83ca45fe24
AhmedMostafaSoliman <ahmedmostafa72@hotmail.com>
2017-07-04 23:50:53 +0200

    added ITCCA

--- assisstant/keyboard/classification/itcca.py
+++ assisstant/keyboard/classification/itcca.py
@@ -1,9 +1,21 @@
 import numpy as np
 from keyboard.preprocessing import butter
+from keyboard.classification.abstract_classifier import AbstractClassifier
+from keyboard.dataset_manager.reader import getUserDatasets
 from . import rcca
+import os
+from assisstant import settings
+
+def classify(self,sample):
+    sample = butter.filter(sample)
+    freqs = np.array(self.freqs)
+    n_harmonics = 3
+    ref = getArtificialRefSignal(freqs, n_harmonics, self.duration, 128)
+    sample = butter.filter(sample)
+    return getBestFrequency(sample, ref)
 
 #Returns a list of matrices where each row in each matrix is a sin or cos with frequency = freqs[i] , the number of harmonics = Nharmonics and row length = sample_length
-def getArtificialRefSignal(freqs,Nharmonics,n_secs,fs):
+def getArtificialRefSignal(self,freqs,Nharmonics,n_secs,fs):
     t = np.linspace(0,n_secs,fs*n_secs)
     mat_list = np.empty(freqs.size,dtype=object)
     for i in range(freqs.size):
@@ -15,8 +27,7 @@ def getArtificialRefSignal(freqs,Nharmonics,n_secs,fs):
         mat_list[i] = Y
     return mat_list
 
-
-def getCorrelationVector(X,Xcap,Y):
+def getCorrelationVector(self,X,Xcap,Y):
     cca = rcca.CCA(kernelcca = False, reg = 0, numCC = 1)
     cca.train([np.transpose(X),np.transpose(Xcap)])
     Wxxcap = cca.ws[0] #WX(X,Xcap)
@@ -25,57 +36,46 @@ def getCorrelationVector(X,Xcap,Y):
     Wxcapy = cca.ws[0] #Wxcap(Xcap,Y)
     cca.train([np.transpose(X),np.transpose(Y)])
     Wxy = cca.ws[0]    #WX(X,Y)
-#    Wyx = cca.ws[1]    #WY(X,Y)
     r1 = cca.cancorrs[0]
     r2 = np.corrcoef(np.dot(Wxxcap.T,X),np.dot(Wxxcap.T,Xcap))[0,1]
-#    r3 = np.corrcoef(np.dot(Wxy.T,X),np.dot(Wxy.T,Xcap))[0,1]
+    #r3 = np.corrcoef(np.dot(Wxy.T,X),np.dot(Wxy.T,Xcap))[0,1]
     r4 = np.corrcoef(np.dot(Wxcapy.T,X),np.dot(Wxcapy.T,Xcap))[0,1]
     r5 = np.corrcoef(np.dot(Wxxcap.T,Xcap),np.dot(Wxcapx.T,Xcap))[0,1]
     return np.array([r1,r2,0,r4,r5])
 
-def getRho(corr_vec):
+def getRho(self,corr_vec):
     rho = 0
     for i in range(5):
         rho += np.sign(corr_vec[i]) * (corr_vec[i]**2)
     return rho
 
-
-def getBestFrequency(eeg_data, ref_data, data, target):
+def getBestFrequency(self,eeg_data, ref_data):
     bestfreq = 0
     bestrho= -100000000
-    for i in range(len(ref_data)):
-        indices = np.where(target == i+1)
-        target_data = data[indices,:,:][0]
-        Xcap = np.mean(target_data,0)
-        Y = ref_data[i]
+    for target_idx in range(len(ref_data)):
+        Xcap = self.load_model(target_idx)
+        Y = ref_data[target_idx]
         vec = getCorrelationVector(eeg_data,Xcap,Y)
         rho = getRho(vec)
         if  rho > bestrho:
             bestrho = rho
-            bestfreq = i
+            bestfreq = target_idx
     return bestfreq
 
-def classify(sample, freqs, duration, old_data = None):
-    data, target = old_data
-    data = data[:,:,0:128*3]
-    data = butter.filter(data)
-    freqs = np.array(freqs)
-    n_harmonics = 3
-    ref = getArtificialRefSignal(freqs, n_harmonics, duration, 128)
-    sample = butter.filter(sample)
-    return getBestFrequency(sample, ref, data, target)
 
+def train(self):
+    data , target = getUserDatasets(settings.USER,settings.TIME_FLASH_SEC)
+    directory = os.path.join('templates', settings.USER)
+    for target_idx in range(4):
+        indices = np.where(target == target_idx)
+        target_data = data[indices, :, :][0]
+        Xcap = np.mean(target_data, 0)
+        filename = settings.USER + '_target'+ str(target_idx)
+        path = os.path.join(directory, filename + ".csv")
+        np.savetxt(path,Xcap,delimiter=",")
 
-
-
-##data, target = read("Amr4", 5)
-#data = butter_bandpass_filter(data)
-
-#correct = 0
-#for i in range(25):
-##    print targett[i]
-    #if(target[i] == ITCCA_classify(data[i,:,:])):
-        #correct += 1
-#    print "-------------------------"
-        
-#print correct
+def load_model(self,target_idx):
+    directory = os.path.join('templates', settings.USER)
+    filename = settings.USER + '_target' + str(target_idx)
+    path = os.path.join(directory, filename + ".csv")
+    return np.loadtxt(path)
